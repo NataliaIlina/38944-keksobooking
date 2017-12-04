4bb8663c75f9cdfc95467ce34f863073e786c368
Natalia Ilina <metlinova86@gmail.com>
2017-12-04 20:30:15 +0300

    Добавлена проверка на валидность формы для Edge

--- js/form.js
+++ js/form.js
@@ -2,6 +2,7 @@
 
 (function () {
   var form = document.querySelector('.notice__form');
+  var address = form.querySelector('#address');
   var timein = form.querySelector('#timein');
   var timeout = form.querySelector('#timeout');
   var type = form.querySelector('#type');
@@ -18,73 +19,86 @@
 
   if (!form.classList.contains('.notice__form--disabled')) {
     // синхронизируем время заезда/выезда
-    timein.addEventListener('change', function () {
-      timeout.value = timein.value;
-    });
-
-    timeout.addEventListener('change', function () {
-      timein.value = timeout.value;
-    });
+    timein.addEventListener('change', onTimeinChange);
+    timeout.addEventListener('change', onTimeoutChange);
 
     // выставляем минимальную цену в зависимости от выбранного типажилья
-    type.addEventListener('change', function () {
-      switch (type.value) {
-        case 'flat':
-          price.min = 1000;
-          break;
-        case 'bungalo':
-          price.min = 0;
-          break;
-        case 'house':
-          price.min = 5000;
-          break;
-        case 'palace':
-          price.min = 10000;
-          break;
-        default:
-          price.min = 0;
-      }
-    });
+    type.addEventListener('change', onTypeChange);
     // количество гостей по количеству комнат
-    roomsNumber.addEventListener('change', function () {
-      switch (roomsNumber.value) {
-        case '1':
-          guestsNumber.value = 1;
-          break;
-        case '2':
-          guestsNumber.value = 2;
-          break;
-        case '3':
-          guestsNumber.value = 3;
-          break;
-        case '100':
-          guestsNumber.value = 0;
-          break;
-        default:
-          guestsNumber.value = 1;
-      }
-    });
-
-    title.addEventListener('invalid', function () {
-      var validity = title.validity;
-      if (validity.tooShort) {
+    roomsNumber.addEventListener('change', onRoomsNumberChange);
+    // ставим сообщения для поля заголовка
+    title.addEventListener('invalid', onTitleInvalid);
+    // проверяем форму при отправке (для Edge)
+    form.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+      if (title.value.length < minLength) {
         title.setCustomValidity(inputError.tooShort);
-      } else if (validity.tooLong) {
-        title.setCustomValidity(inputError.tooLong);
-      } else if (validity.valueMissing) {
-        title.setCustomValidity(inputError.noValue);
+      } else if (!address.value) {
+        address.style.outline = '2px solid red';
       } else {
-        title.setCustomValidity('');
+        form.submit();
       }
     });
+  }
 
-    title.addEventListener('input', function (evt) {
-      var target = evt.target;
-      if (target.value.length < minLength) {
-        target.setCustomValidity(inputError.tooShort);
-      } else {
-        target.setCustomValidity('');
-      }
-    });
+
+  // обработчики событий изменения полей формы
+  function onTimeinChange() {
+    timeout.value = timein.value;
+  }
+
+  function onTimeoutChange() {
+    timein.value = timeout.value;
+  }
+
+  function onTypeChange() {
+    switch (type.value) {
+      case 'flat':
+        price.min = 1000;
+        break;
+      case 'bungalo':
+        price.min = 0;
+        break;
+      case 'house':
+        price.min = 5000;
+        break;
+      case 'palace':
+        price.min = 10000;
+        break;
+      default:
+        price.min = 0;
+    }
+  }
+
+  function onRoomsNumberChange() {
+    switch (roomsNumber.value) {
+      case '1':
+        guestsNumber.value = 1;
+        break;
+      case '2':
+        guestsNumber.value = 2;
+        break;
+      case '3':
+        guestsNumber.value = 3;
+        break;
+      case '100':
+        guestsNumber.value = 0;
+        break;
+      default:
+        guestsNumber.value = 1;
+    }
+  }
+
+  function onTitleInvalid() {
+    var validity = title.validity;
+    if (validity.tooShort) {
+      title.setCustomValidity(inputError.tooShort);
+    } else if (validity.tooLong) {
+      title.setCustomValidity(inputError.tooLong);
+    } else if (validity.valueMissing) {
+      title.setCustomValidity(inputError.noValue);
+    } else {
+      title.setCustomValidity('');
+    }
   }
 })();
